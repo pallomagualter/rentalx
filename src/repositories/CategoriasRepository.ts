9e84dff203804125b445a4c7f2fb9d6d5b345c2f
pallomagualter <pallomagualter@gmail.com>
2021-11-29 12:52:56 -0300

    repository

---
+++ src/repositories/CategoriasRepository.ts
@@ -0,0 +1,29 @@
+// DTO - Data Transfer Object
+import { Category } from "../model/Category";
+
+interface ICreateCategoryDTO {
+    name: string;
+    description: string;
+}
+
+class CategoryRepository {
+    private categories: Category[];
+
+    constructor() {
+        this.categories = [];
+    }
+
+    create({ description, name }: ICreateCategoryDTO): void {
+        const category = new Category();
+
+        Object.assign(category, {
+            name,
+            description,
+            created_at: new Date()
+        });
+    
+        this.categories.push(category);
+    }
+}
+
+export { CategoryRepository };
