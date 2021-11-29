95f082889cd27b6a5623a5f5d0f69002f8aaf910
pallomagualter <pallomagualter@gmail.com>
2021-11-29 12:11:22 -0300

    model

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,14 +1,15 @@
 import { Router } from "express";
 import { v4 as uuidV4 } from "uuid";
+import { Category } from "../model/Category";
 
 const categoriesRoutes = Router();
 
-const categories = [];
+const categories: Category[] = [];
 
 categoriesRoutes.post("/", (request, response) => {
     const { name, description } = request.body;
 
-    const category = {
+    const category: Category = {
         id: uuidV4(),
         name,
         description,
