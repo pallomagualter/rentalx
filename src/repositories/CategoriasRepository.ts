6d68e9880d32149eb250c7697f383c108dd9f303
pallomagualter <pallomagualter@gmail.com>
2021-11-29 13:12:24 -0300

    validation if category already exists

--- src/repositories/CategoriasRepository.ts
+++ src/repositories/CategoriasRepository.ts
@@ -24,6 +24,16 @@ class CategoryRepository {
     
         this.categories.push(category);
     }
+
+    list(): Category[] {
+        return this.categories;
+    }
+
+    findByName(name: string): Category {
+        const category = this.categories.find(category => category.name === name);
+
+        return category;
+    }
 }
 
 export { CategoryRepository };
