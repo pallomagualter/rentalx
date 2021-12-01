e8bc865d9af0537381626a84e53c6c1f89e37d51
pallomagualter <pallomagualter@gmail.com>
2021-12-01 12:05:08 -0300

    Specification Repository

---
+++ src/modules/cars/repositories/SpecificationRepository.ts
@@ -0,0 +1,14 @@
+import { Specification } from "../model/Specification";
+import { ISpecificationsRepository, ICreateSpecificationDTO } from "./ISpecificationsRepository";
+
+
+class SpecificationsRepository implements ISpecificationsRepository {
+    private specifications: Specification[]
+
+    create({ name, description }: ICreateSpecificationDTO): void {
+        throw new Error("Method not implemented.");
+    }
+
+}
+
+export { SpecificationsRepository }
