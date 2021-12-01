1d098d4b8524fac19ea8bacf6316cc7aaa400fb0
pallomagualter <pallomagualter@gmail.com>
2021-12-01 09:59:46 -0300

    specifications

---
+++ src/modules/cars/repositories/ISpecificationsRepository.ts
@@ -0,0 +1,12 @@
+
+
+interface ICreateSpecificationDTO {
+    name: string;
+    description: string;
+}
+
+interface ISpecificationsRepository {
+    create ({ name, description }: ICreateSpecificationDTO): void;
+}
+
+export { ISpecificationsRepository };
