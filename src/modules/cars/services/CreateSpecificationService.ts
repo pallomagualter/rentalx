a3b225c62d2d06214edea319cb22508c5c4386cc
pallomagualter <pallomagualter@gmail.com>
2021-12-01 13:00:54 -0300

    Specification Service

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,8 +1,20 @@
+import { ISpecificationsRepository } from "../repositories/ISpecificationsRepository";
 
+interface IRequest {
+    name: string;
+    description: string;
+}
 
 class CreateSpecificationService {
-    execute(){
-        console.log("TO DO")
+    constructor(private specificationsRepository: ISpecificationsRepository) {
+
+    }
+
+    execute({name, description}: IRequest): void {
+        this.specificationsRepository.create({
+            name,
+            description,
+        });
     }
 }
 
