21b7871ccd813f5341eb66ded131a02c3d6b20a2
pallomagualter <pallomagualter@gmail.com>
2021-12-07 11:02:32 -0300

    CreateCategoryController

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -7,13 +7,7 @@ const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
-    const { name, description } = request.body;
-
-    const createCategoryService = new CreateCategoryService(categoriesRepository);
-
-    createCategoryService.execute({ name, description });
-
-    return response.status(201).send();
+    
 });
 
 categoriesRoutes.get("/", (request, response) => {
