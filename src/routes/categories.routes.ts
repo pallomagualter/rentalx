aa8991c083ae9e495cc4ebd96ed4b5fe121b0723
pallomagualter <pallomagualter@gmail.com>
2021-12-17 13:26:29 -0300

    multer

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
-
-import { CategoriesRepository } from "../modules/cars/repositories/implementations/CategoriesRepository";
+import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
+
+const upload = multer({
+    dest: "./tmp",
+});
 
 categoriesRoutes.post("/", (request, response) => {
     return createCategoryController.handle(request, response);
@@ -15,6 +17,12 @@ categoriesRoutes.post("/", (request, response) => {
 categoriesRoutes.get("/", (request, response) => {
     return listCategoriesController.handle(request, response);
 
+});
+
+categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
+    const { file } = request;
+    console.log(file);
+    return response.send();
 });
 
 export { categoriesRoutes };
