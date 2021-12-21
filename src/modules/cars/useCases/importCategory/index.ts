55e8f8eec4d28fdb3396249a686cec39d4e9d0d0
pallomagualter <pallomagualter@gmail.com>
2021-12-21 15:37:20 -0300

    corrigindo versão csv-parse

--- src/modules/cars/useCases/importCategory/index.ts
+++ src/modules/cars/useCases/importCategory/index.ts
@@ -7,5 +7,4 @@ const categoriesRepository = CategoriesRepository.getInstance();
 const importCategoryUseCase = new ImportCategoryUseCase(categoriesRepository);
 const importCategoryController = new ImportCategoryController(importCategoryUseCase);
 
-
 export { importCategoryController }
